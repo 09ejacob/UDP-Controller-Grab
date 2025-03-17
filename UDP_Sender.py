5a6d8305036959c4e07751efdb1eecca6d3697ad
Elias Garseth Jacobsen <elias.garseth.jacobsen@gmail.com>
2025-03-17 14:57:38 +0100

    Renamed files and made a very simple test sender script for sending one move command. This was like the one used before the slider script.

---
+++ UDP_Sender.py
@@ -0,0 +1,13 @@
+import socket
+import time
+
+def send_udp_command(message, host='127.0.0.1', port=9999):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+        udp_socket.sendto(message.encode('utf-8'), (host, port))
+        print(f"Sent command: {message}")
+
+if __name__ == '__main__':
+    #while True:
+    send_udp_command("axis:1:0")
+    #send_udp_command("axis:2:90.0")
+        #time.sleep(0.1)
