1521ec8db4bf6a777a6548600e3b7de67d71248a
Elias Garseth Jacobsen <elias.garseth.jacobsen@gmail.com>
2025-03-19 09:52:33 +0100

    Added a defaultScenario which now sends commands to read force sensor value. Also added a test scenario so we can test if the receiver is receiving properly.

--- UDP_Sender.py
+++ UDP_Sender.py
@@ -6,8 +6,21 @@ def send_udp_command(message, host='127.0.0.1', port=9999):
         udp_socket.sendto(message.encode('utf-8'), (host, port))
         print(f"Sent command: {message}")
 
-if __name__ == '__main__':
+def test():
+    send_udp_command("axis:2:0.5")
+
+def defaultScenario():
     #while True:
-    send_udp_command("axis:1:0")
-    #send_udp_command("axis:2:90.0")
-        #time.sleep(0.1)
+        send_udp_command("axis:2:0.5")
+        #send_udp_command("axis:2:90.0")
+        time.sleep(3.0)
+        send_udp_command("force_data")
+        time.sleep(3.0)
+        send_udp_command("axis:2:0.0")
+        time.sleep(3.0)
+        send_udp_command("force_data")
+
+
+if __name__ == '__main__':
+    test()
+    #defaultScenario()
