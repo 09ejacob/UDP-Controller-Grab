6e560591329bbe3849318007d7573b171d0a4c94
Elias Garseth Jacobsen <elias.garseth.jacobsen@gmail.com>
2025-03-25 10:38:49 +0100

    Added previously used udp sender as a test file for testing purposes.

--- UDP_Sender.py
+++ UDP_Sender.py
@@ -17,31 +17,33 @@ def send_commands_at_rate(commands, host='127.0.0.1', port=9999, frequency=200):
 
 
 # Possible commands are:
-# axis:x:x - Moves axis x to location x
+# axisx:x - Moves axis x to location x
 # force_data - Makes robot controller print out force sensor data
 # close_gripper - Closes the gripper
 # open_gripper - Opens the gripper
+# tp_robot:x:y:z - Teleports the robot to given x, y, and z coordinates
 if __name__ == '__main__':
     commands = [
-        "axis:2:0.51",
-        "axis:2:0.52",
-        "axis:2:0.53",
-        "axis:2:0.54",
-        "axis:2:0.55",
-        "axis:2:0.56",
-        "axis:2:0.57",
-        "axis:2:0.58",
-        "axis:2:0.59",
-        "axis:2:0.60",
-        "axis:2:0.61",
-        "axis:2:0.62",
-        "axis:2:0.63",
-        "axis:2:0.64",
-        "axis:2:0.65",
-        "axis:2:0.66",
-        "axis:2:0.67",
-        "axis:2:0.68",
-        "axis:2:0.69",
-        "axis:2:0.70",
+        "axis2:0.51",
+        "axis2:0.52",
+        "axis2:0.53",
+        "axis2:0.54",
+        "axis2:0.55",
+        "axis2:0.56",
+        "axis2:0.57",
+        "axis2:0.58",
+        "axis2:0.59",
+        "axis2:0.60",
+        "axis2:0.61",
+        "axis2:0.62",
+        "axis2:0.63",
+        "axis2:0.64",
+        "axis2:0.65",
+        "axis2:0.66",
+        "axis2:0.67",
+        "axis2:0.68",
+        "axis2:0.69",
+        "axis2:0.70",
+        # "tp_robot:-2:-2:0"
     ]
     send_commands_at_rate(commands, frequency=10)
---
+++ test_UDP_sender.py
@@ -0,0 +1,48 @@
+import socket 
+import time
+
+def send_udp_command(message, host='127.0.0.1', port=9999):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+        udp_socket.sendto(message.encode('utf-8'), (host, port))
+ 
+        print(f"Sent command: {message}")
+
+def test(): 
+    send_udp_command("axis:2:1.5")
+    #send_udp_command("axis:2:1.5")
+    send_udp_command("tp_robot:-1:-2:0")
+
+def default_scenario(): 
+    send_udp_command("axis:1:90")
+    time.sleep(3.0)
+    send_udp_command("axis:2:0.5")
+    time.sleep(3.0)
+    send_udp_command("axis:3:-1.45")
+    time.sleep(3.0)
+    force_sensor()
+    time.sleep(1)
+    send_udp_command("axis:2:0.05")
+    time.sleep(3.0)
+    send_udp_command("close_gripper")
+    time.sleep(1)
+    send_udp_command("axis:2:0.75")
+    time.sleep(3)
+    force_sensor()
+    time.sleep(1)
+    send_udp_command("axis:1:0")
+    time.sleep(3)
+    send_udp_command("open_gripper")
+
+def force_sensor(): 
+    send_udp_command("force_data")
+
+if __name__ == '__main__': 
+    # while True:
+    #     force_sensor()
+    #     time.sleep(0.1)
+    #test()
+    # while True:
+    #     force_sensor()
+    #     time.sleep(0.1)
+    #test()
+    default_scenario()
