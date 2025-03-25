0fb6cb36d95bca9b446ace1c7285bfe4691b6473
Elias Garseth Jacobsen <elias.garseth.jacobsen@gmail.com>
2025-03-25 09:32:18 +0100

    Updated the script to send at a given rate/hz (should be 100-200). Can add move commands to the commands and then the script will go through all the commands at the given frequency.

--- UDP_Sender.py
+++ UDP_Sender.py
@@ -1,41 +1,47 @@
 import socket
 import time
 
-def send_udp_command(message, host='127.0.0.1', port=9999):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-        udp_socket.sendto(message.encode('utf-8'), (host, port))
-        print(f"Sent command: {message}")
+def send_udp_command(udp_socket, message, host, port):
+    udp_socket.sendto(message.encode('utf-8'), (host, port))
+    print(f"Sent command: {message}")
 
-def test():
-    #send_udp_command("axis:2:1.5")
-    send_udp_command("tp_robot:-1:-2:0")
+def send_commands_at_rate(commands, host='127.0.0.1', port=9999, frequency=200): # Frequency is the hz we send commands at
+    interval = 1.0 / frequency
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        for command in commands:
+            send_udp_command(udp_socket, command, host, port)
+            time.sleep(interval)
+    finally:
+        udp_socket.close()
 
-def default_scenario():
-    send_udp_command("axis:1:90")
-    time.sleep(3.0)
-    send_udp_command("axis:2:0.5")
-    time.sleep(3.0)
-    send_udp_command("axis:3:-1.45")
-    time.sleep(3.0)
-    force_sensor()
-    time.sleep(1)
-    send_udp_command("axis:2:0.05")
-    time.sleep(3.0)
-    send_udp_command("close_gripper")
-    time.sleep(1)
-    send_udp_command("axis:2:0.75")
-    time.sleep(3)
-    force_sensor()
-    time.sleep(1)
-    send_udp_command("axis:1:0")
-    time.sleep(3)
-    send_udp_command("open_gripper")
 
-def force_sensor():
-    send_udp_command("force_data")
-
+# Possible commands are:
+# axis:x:x - Moves axis x to location x
+# force_data - Makes robot controller print out force sensor data
+# close_gripper - Closes the gripper
+# open_gripper - Opens the gripper
 if __name__ == '__main__':
-    # while True:
-    #     force_sensor()
-    #     time.sleep(0.1)
-    test()
+    commands = [
+        "axis:2:0.51",
+        "axis:2:0.52",
+        "axis:2:0.53",
+        "axis:2:0.54",
+        "axis:2:0.55",
+        "axis:2:0.56",
+        "axis:2:0.57",
+        "axis:2:0.58",
+        "axis:2:0.59",
+        "axis:2:0.60",
+        "axis:2:0.61",
+        "axis:2:0.62",
+        "axis:2:0.63",
+        "axis:2:0.64",
+        "axis:2:0.65",
+        "axis:2:0.66",
+        "axis:2:0.67",
+        "axis:2:0.68",
+        "axis:2:0.69",
+        "axis:2:0.70",
+    ]
+    send_commands_at_rate(commands, frequency=10)
